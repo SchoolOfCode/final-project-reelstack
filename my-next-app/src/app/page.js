d55e6068e85dbc95f230bd654be65513618f323c
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-09-24 10:12:18 +0100

    test commit husky

--- my-next-app/src/app/page.js
+++ my-next-app/src/app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
+const unusedVariable = 'I am not used anywhere';
+
 export default function Home() {
   return (
     <div className={styles.page}>
