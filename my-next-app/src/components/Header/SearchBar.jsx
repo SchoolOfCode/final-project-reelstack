65015a4223d69fbb8e68c4d5e1f49f5cf330f4f3
Shilpa <eclectic89@hotmail.com>
2024-09-25 23:04:50 +0100

    scroll bar for reviews in MoviePage

--- my-next-app/src/components/Header/SearchBar.jsx
+++ my-next-app/src/components/Header/SearchBar.jsx
@@ -1,10 +1,10 @@
 // my-next-app/src/components/SearchBar.jsx
-'use client'
-import styles from "./SearchBar.module.css";
+'use client';
+import styles from './SearchBar.module.css';
 import { useState, useEffect, useRef } from 'react';
 
 export default function SearchBar({ onClose }) {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const inputRef = useRef(null);
 
   useEffect(() => {
@@ -20,33 +20,31 @@ export default function SearchBar({ onClose }) {
 
   return (
     <form className={styles.searchBar} onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        value={query} 
-        onChange={(e) => setQuery(e.target.value)} 
-        placeholder="Search..." 
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search..."
         className={styles.input}
         ref={inputRef}
       />
-      <button 
-        type="button" 
-        className={styles.closeButton} 
+      <button
+        type="button"
+        className={styles.closeButton}
         onClick={onClose}
         aria-label="Close search"
       >
-      <svg xmlns="http://www.w3.org/2000/svg" 
-      height="24px" 
-      viewBox="0 -960 960 960" 
-      width="24px" 
-      fill="#ff6d00">
-      <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#ff6d00"
+        >
+          <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+        </svg>
       </button>
-      <button 
-        type="submit" 
-        className={styles.submitButton}
-        aria-label="Submit search"
-      >
+      <button type="submit" className={styles.submitButton} aria-label="Submit search">
         {/* icon removed */}
       </button>
     </form>
