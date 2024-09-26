f4e8116cfc3e3b7f2ff26db90a69792349feb80b
Shilpa <eclectic89@hotmail.com>
2024-09-26 14:10:54 +0100

    Logo Change

--- my-next-app/src/components/Header/Header.jsx
+++ my-next-app/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@
 import styles from './Header.module.css'; // Import CSS Modules
 import { useState } from 'react';
 import SearchBar from './SearchBar.jsx';
+import Image from 'next/image';
 
 export default function Header() {
   const [isSearchActive, setIsSearchActive] = useState(false);
@@ -15,29 +16,42 @@ export default function Header() {
   };
 
   return (
-    <header className={`${styles.header} ${isSearchActive ? styles.searchActiveHeader : ''}`}>
-      <h1 className={`${styles.title} ${isSearchActive ? styles.hidden : ''}`}>📽Reel Magic📽</h1>
-      <button
-        aria-label="Search"
-        className={`${styles.searchButton} ${isSearchActive ? styles.searchActive : ''}`}
-        onClick={!isSearchActive ? handleSearchClick : undefined}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle className={styles.iconStroke} cx="10" cy="10" r="7" />
-          <line className={styles.iconStroke} x1="21" y1="21" x2="15" y2="15" />
-        </svg>
-      </button>
-      {isSearchActive && <SearchBar onClose={handleCloseSearch} />}
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/reelstacklcolor.png"
+          alt="Reel Magic Logo"
+          width={35}
+          height={35}
+          className={styles.logo}
+        />
+        <h1 className={styles.title}> Reel Magic </h1>
+      </div>
+      <div className={styles.searchContainer}>
+        {!isSearchActive && (
+          <button
+            aria-label="Search"
+            className={`${styles.searchButton} ${isSearchActive ? styles.searchActive : ''}`}
+            onClick={!isSearchActive ? handleSearchClick : undefined}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle className={styles.iconStroke} cx="10" cy="10" r="7" />
+              <line className={styles.iconStroke} x1="21" y1="21" x2="15" y2="15" />
+            </svg>
+          </button>
+        )}
+        {isSearchActive && <SearchBar onClose={handleCloseSearch} />}
+      </div>
     </header>
   );
 }
