b42cdf6bdc1f272ab629cb9c7da79396430e741a
Shilpa <eclectic89@hotmail.com>
2024-09-26 16:13:37 +0100

    tm

---
+++ my-next-app/src/components/Footer/teamMembers.js
@@ -0,0 +1,35 @@
+const teamMembers = [
+  {
+    name: "James O'kane",
+    github: 'https://github.com/Orbiccode',
+    linkedin: 'https://www.linkedin.com/in/james-o-kane-570b71314/',
+  },
+  {
+    name: 'Amin Abdikadir',
+    github: 'https://github.com/Amin-Abdikadir',
+    linkedin: 'https://www.linkedin.com/in/amin-abdikadir-b12267297/',
+  },
+  {
+    name: 'Rowan Godfrey',
+    github: 'https://github.com/rw4n99',
+    linkedin: 'https://www.linkedin.com/in/rowangodfrey/',
+  },
+  {
+    name: 'Maria Gadkova',
+    github: 'https://github.com/MariSayo',
+    linkedin: 'https://linkedin.com/in/maria-gadkova-a23643300',
+  },
+  {
+    name: 'Shilpa Goel',
+    github: 'https://github.com/SG801',
+    linkedin: 'https://www.linkedin.com/in/shilpa-g-75a1991a/',
+  },
+
+  {
+    name: 'Oliver Kelly',
+    github: 'https://github.com/olikelly00',
+    linkedin: 'https://linkedin.com/in/olikelly/',
+  },
+];
+
+export default teamMembers;
